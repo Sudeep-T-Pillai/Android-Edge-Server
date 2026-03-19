7d66a834f148f319b536a26ca5391f68053dc487
Sudeep T Pillai <sudeep266133@gmail.com>
2026-03-19 16:53:40 +0530

    Added star like animation, still needs improvment though git add .git add .git add .!

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,53 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-serif dark:bg-black  overflow-hidden">
-      <main className="relative flex min-h-screen w-full max-w-4xl flex-col justify-center bg-white dark:bg-black sm:items-center ">
-        <div className="flex flex-col items-center row gap-4 ">
+    <div className="flex min-h-screen items-center justify-center bg-black font-serif overflow-hidden">
+        {/* Stars */}
+<div className="flex min-h-screen items-center justify-center bg-black font-serif overflow-hidden">
+
+  {/* Stars background grid
+  <div className="absolute inset-0 
+    bg-[radial-gradient(circle,white_1px,transparent_1px)] 
+    bg-[length:40px_40px] opacity-20">
+  </div> */}
+
+  {/* Glowing stars */}
+  {[...Array(20)].map((_, i) => (
+    <span
+      key={i}
+      className="absolute w-1 h-1 bg-white rounded-full animate-ping"
+      style={{
+        top: `${Math.random() * 100}%`,
+        left: `${Math.random() * 100}%`,
+        animationDelay: `${Math.random() * 3}s`,
+        animationDuration: `${1 + Math.random() * 2}s`,
+      }}
+    />
+  ))}
+
+</div>
+
+      <main className="relative flex min-h-screen w-full max-w-4xl flex-col justify-center sm:items-center">
+        {/* Content */}
+        <div className="relative z-10 flex flex-col items-center gap-4 text-white">
 
           <h1 className="text-8xl text-center">
-            Hey there,  
+            Hey there,
           </h1>
-          <h2 className="text-4xl text-center" >
-            I'm Sudeep, Something is being built here. Not ready yet  but neither am I, and that's never stopped me
+
+          <h2 className="text-4xl text-center">
+            I'm Sudeep, Something is being built here. Not ready yet but neither am I, and that's never stopped me
           </h2>
-            <a href="https://github.com/Sudeep-T-Pillai" target="_blank" rel="noopener noreferrer">
+
+          <a href="https://github.com/Sudeep-T-Pillai">
             Visit GitHub
           </a>
-            <a href="https://www.linkedin.com/in/sudeep-t-pillai" target="_blank" rel="noopener noreferrer">
+
+          <a href="https://www.linkedin.com/in/sudeep-t-pillai">
             Visit LinkedIn
-         </a>
+          </a>
+
         </div>
+
       </main>
     </div>
   );
